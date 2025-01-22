95dcd0134b0804119df2efa8fbc0d788cbe08ca5
Struan Brown <struan@tonnard.co.uk>
2025-01-22 20:40:41 +0000

    Added comments

--- consts.py
+++ consts.py
@@ -1,2 +1,2 @@
 ORIGIN = (0,0)
-SPRITE_SCALE = 1
+SPRITE_SCALE = 1 #sets scale of sprites, default is 1
--- spritesheet.py
+++ spritesheet.py
@@ -1,15 +1,17 @@
 import pygame 
 import consts
 
+# class used to extract and play animations from a spritesheet 
 class Spritesheet():
     def __init__(self, image, colour):
-        self.sheet = image
-        self.colour = colour
+        self.sheet = image 
+        self.colour = colour 
         self.scale = consts.SPRITE_SCALE
-        self.frameCounter = 0
-        self.animationCounter = 0
-        self.startUpCounter = 0
+        self.frameCounter = 0 
+        self.animationCounter = 0 
+        self.startUpCounter = 0 
 
+    # extracts a single frame from a spritesheet, flipping vertically or horizontally if necessary
     def get_frame(self, frame_x, frame_y, width, height, flip_x, flip_y):
         image = pygame.Surface((width,height)).convert_alpha()
         image.blit(self.sheet, consts.ORIGIN, ((frame_x * width), (frame_y * height), width, height))
@@ -20,6 +22,7 @@ class Spritesheet():
     
         return image
 
+    # retruns a series of frames as a list
     def get_frames(self, frameHeight, framesLength, width, height, flipX = 0, flipY = 0):
         frames = []
         for i in range(framesLength):
@@ -27,6 +30,7 @@ class Spritesheet():
             frames.append(frame)
         return frames
 
+    # blits a list of frames to a given surface as an animation, animationTimer controls the speed, startUpPause can be used to insert a delay before the animation plays again
     def play_animation(self, screen, pos, frames, animationTimer, startUpPause = 0):
         screen.blit(frames[self.frameCounter], pos)
         self.startUpCounter  += 1
